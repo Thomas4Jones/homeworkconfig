900d6355ba4defe15b18bc38863d89e783e1ba2f
Thomas4Jones <btk1400s@gmail.com>
2024-03-12 21:08:46 +0700

    initialcommit

---
+++ PycharmProjects/pythonProject9martahomework/config/config2.py
@@ -0,0 +1,22 @@
+import os
+import os.path
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from config.action import SeleniumAction
+
+
+@pytest.fixture
+def browser():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    gecko_path = os.path.join(current_dir, "geckodriver.exe")
+    web_service = Service(gecko_path)
+    driver = webdriver.Firefox(service=web_service)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def selenium_action(browser):
+    selenium_action = SeleniumAction(browser)
+    yield selenium_action
---
+++ PycharmProjects/pythonProject9martahomework/test_bankxyz/data.py
@@ -0,0 +1,6 @@
+data_web_adres = ("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login") # изменил
+data_deposit = "1000000"
+data_withdrawl = "500324"
+data_first_name = "Colin"
+data_last_name = "Farell"
+data_post_code = "63051"
